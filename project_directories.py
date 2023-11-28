33c976ad2e2761622be2d5df2139d18b252d3087
debrevitatevitae <giotb92@gmail.com>
2023-11-28 18:23:48 +0100

    project_directories: update

--- project_directories.py
+++ project_directories.py
@@ -1,6 +1,7 @@
-import os
 from pathlib import Path
-import sys
 
 DATA_DIR = Path() / 'data'
-OUTPUT_DIR = Path() / 'output'
+RAW_DATA_DIR = DATA_DIR / 'raw'
+PICKLE_DATA_DIR = DATA_DIR / 'pickle'
+PROC_DATA_DIR = DATA_DIR / 'processed'
+GRAPHICS_DIR = Path() / 'graphics'
