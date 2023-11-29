430f221511d74edf27399cc91be0d41df6728982
debrevitatevitae <giotb92@gmail.com>
2023-11-29 14:38:20 +0100

    rbf_kta_opt.py: adapt for jax
    - rbf kernel function
    - arrays
    - kta function

--- rbf_kta_opt.py
+++ rbf_kta_opt.py
@@ -1,3 +1,5 @@
+import jax
+from jax import numpy as jnp
 import pandas as pd
 import pennylane as qml
 from pennylane import numpy as np
@@ -5,11 +7,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 from project_directories import PROC_DATA_DIR
+from utils import target_alignment
 
 
+@jax.jit
 def rbf_kernel(x, y, gamma=1.):
     """
-    Compute the Gaussian kernel between two vectors.
+    Compute the Gaussian kernel between two vectors. Uses jax.
 
     Parameters
     ----------
@@ -33,32 +37,33 @@ def rbf_kernel(x, y, gamma=1.):
 
     Examples
     --------
-    >>> x = np.array([1, 2, 3])
-    >>> y = np.array([4, 5, 6])
+    >>> x = jnp.array([1, 2, 3])
+    >>> y = jnp.array([4, 5, 6])
     >>> gamma_value = 0.1
     >>> result = gaussian_kernel(x, y, gamma=gamma_value)
     >>> print(result)
     Gaussian Kernel: 0.9875774860176064
     """
-    diff = np.subtract(x, y)
-    norm_squared = np.dot(diff, diff)
-    return np.exp(-gamma*norm_squared)
+    diff = jnp.subtract(x, y)
+    norm_squared = jnp.dot(diff, diff)
+    return jnp.exp(-gamma*norm_squared)
 
 
 if __name__ == '__main__':
+    jax.config.update("jax_enable_x64", True)
+
     np.random.seed(42)
 
     df = pd.read_csv(PROC_DATA_DIR / 'data_labeled.csv')
     X = df[['eps11', 'eps22', 'eps12']].to_numpy()
     y = df['failed'].to_numpy(dtype=np.int32)
 
-    X = np.array(X, requires_grad=False)
-    y = np.array(y, requires_grad=False)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.9)
 
     scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
+    X_train_scaled = scaler.fit_transform(X_train)
 
-    kta = qml.kernels.target_alignment(
-        X_scaled, y, rbf_kernel, assume_normalized_kernel=True)
+    kta_initial = target_alignment(
+        X_train_scaled, y_train, rbf_kernel, assume_normalized_kernel=True)
 
-    print(kta)
+    print(kta_initial)
