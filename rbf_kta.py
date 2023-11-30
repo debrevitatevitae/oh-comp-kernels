f4d23a12a24bd703ce0e9762b98efac8db0b1696
debrevitatevitae <giotb92@gmail.com>
2023-11-30 09:48:15 +0100

    Replace JAX with PennyLane

--- rbf_kta.py
+++ rbf_kta.py
@@ -1,16 +1,10 @@
-import jax
-from jax import numpy as jnp
-import pandas as pd
 import pennylane as qml
 from pennylane import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-from project_directories import PROC_DATA_DIR
 from utils import load_split_data, target_alignment
 
 
-@jax.jit
 def rbf_kernel(x, y, gamma=1.):
     """
     Compute the Gaussian kernel between two vectors. Uses jax.
@@ -44,14 +38,12 @@ def rbf_kernel(x, y, gamma=1.):
     >>> print(result)
     Gaussian Kernel: 0.9875774860176064
     """
-    diff = jnp.subtract(x, y)
-    norm_squared = jnp.dot(diff, diff)
-    return jnp.exp(-gamma*norm_squared)
+    diff = np.subtract(x, y)
+    norm_squared = np.dot(diff, diff)
+    return np.exp(-gamma*norm_squared)
 
 
 if __name__ == '__main__':
-    jax.config.update("jax_enable_x64", True)
-
     np.random.seed(42)
 
     X_train, X_test, y_train, y_test = load_split_data(test_size=0.9)
@@ -59,7 +51,7 @@ if __name__ == '__main__':
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
 
-    kta_initial = target_alignment(
+    kta_initial = qml.kernels.target_alignment(
         X_train_scaled, y_train, rbf_kernel, assume_normalized_kernel=True)
 
     print(kta_initial)
