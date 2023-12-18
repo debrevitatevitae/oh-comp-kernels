a25027ed3bca0e1bbaff1d814f983d42b5039dbc
debrevitatevitae <giotb92@gmail.com>
2023-12-18 11:12:29 +0100

    Update sklearn model selection and preprocessing imports

--- q_kern_accuracy_cv.py
+++ q_kern_accuracy_cv.py
@@ -8,8 +8,8 @@ from jax import numpy as jnp
 import numpy as np
 import pandas as pd
 import pennylane as qml
-from sklearn.model_selection import cross_val_score
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.model_selection import GridSearchCV, cross_val_score
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVC
 from project_directories import PICKLE_DATA_DIR, RESULTS_DIR
 
@@ -73,15 +73,8 @@ def layer(x, params, wires, i0=0, inc=1):
 
 def embedding(x, params, wires):
     """Adapted from https://github.com/thubregtsen/qhack"""
-    inc = 1
     for j, layer_params in enumerate(params):
         layer(x, layer_params, wires, i0=j * len(wires))
-    # encode data one last time to avoid cancellations in the kernel circuit
-        i = len(params) * len(wires)
-        for wire in wires:
-            qml.Hadamard(wires=[wire])
-            qml.RZ(x[i % len(x)], wires=[wire])
-            i += inc
 
 
 if __name__ == '__main__':
@@ -104,16 +97,16 @@ if __name__ == '__main__':
     y_test = jnp.array(y_test)
 
     # define the embedding kernel (with JAX)
-    num_qubits = 9
-    num_layers = 4
+    num_qubits = 3
+    num_layers = 2
     wires = list(range(num_qubits))
     dev = qml.device('default.qubit.jax', wires=num_qubits)
 
     # random params or load them from pkl file
-    load_params = True
+    load_params = False
 
     if load_params:
-        with open(PICKLE_DATA_DIR / f"q_kern_kta_opt_0{num_qubits}0{num_layers}.pkl", 'rb') as params_file:
+        with open(PICKLE_DATA_DIR / f"q_kern_kta_opt_he2w{num_qubits}d{num_layers}.pkl", 'rb') as params_file:
             params = pickle.load(params_file)
     else:
         _, key = jax.random.split(key)
@@ -129,14 +122,15 @@ if __name__ == '__main__':
         return qml.expval(qml.Projector([0]*num_qubits, wires=wires))
 
     # Define the parameter grid for GridSearchCV
-    cv_param_grid = np.logspace(-1, 2, 4)
+    cv_param_grid = {
+        "C": np.logspace(-1, 2, 4)
+    }
 
     # create SVC
     svc = SVC(kernel=partial(qml.kernels.kernel_matrix, kernel=kernel))
 
     # create a GridSearchCV and fit to the data
-    grid_search = GridSearchCVCustom(
-        estimator=svc, param_grid=cv_param_grid, batch_size=int(0.2*num_samples), cv=10, n_jobs=-1)
+    grid_search = GridSearchCV(svc, cv_param_grid, cv=5, n_jobs=-1, verbose=3)
 
     grid_search.fit(X_train_scaled, y_train)
 
@@ -146,7 +140,7 @@ if __name__ == '__main__':
     python_file_name = os.path.basename(__file__)
     python_file_name_no_ext = os.path.splitext(python_file_name)[0]
     file_name_width_depth = python_file_name_no_ext + \
-        f"_0{num_qubits}0{num_layers}"
+        f"_he2w{num_qubits}d{num_layers}"
     results_file_name = file_name_width_depth + \
         "_trained" if load_params else file_name_width_depth + "_random"
     df_results.to_csv(
