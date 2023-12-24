03aa63a704a688bb73bd50c5795fc46739d1780b
debrevitatevitae <giotb92@gmail.com>
2023-12-24 16:36:33 +0100

    Replace embedding function and update data extension

--- q_kern_accuracy_cv.py
+++ q_kern_accuracy_cv.py
@@ -17,25 +17,6 @@ from project_directories import PICKLE_DATA_DIR, PROC_DATA_DIR, RESULTS_DIR
 jax.config.update('jax_enable_x64', False)
 
 
-def layer(x, params, wires, i0=0, inc=1):
-    """Taken from https://github.com/thubregtsen/qhack"""
-    i = i0
-    for j, wire in enumerate(wires):
-        qml.Hadamard(wires=[wire])
-        qml.RZ(x[i % len(x)], wires=[wire])
-        i += inc
-        qml.RY(params[0, j], wires=[wire])
-
-    qml.broadcast(unitary=qml.CRZ, pattern="ring",
-                  wires=wires, parameters=params[1])
-
-
-def embedding(x, params, wires):
-    """Adapted from https://github.com/thubregtsen/qhack"""
-    for j, layer_params in enumerate(params):
-        layer(x, layer_params, wires, i0=j * len(wires))
-
-
 if __name__ == '__main__':
     start = time.time()
 
@@ -51,33 +32,30 @@ if __name__ == '__main__':
     scaler = MinMaxScaler(feature_range=(0, np.pi))
     X_train_scaled = scaler.fit_transform(X_train)
 
+    num_qubits = 3
+    # Match X_train_scaled.shape[1] to num_qubits by cycling the columns of X_train_scaled
+    X_train_scaled = np.hstack(
+        [X_train_scaled[:, i % X_train_scaled.shape[1]].reshape(-1, 1) for i in range(num_qubits)])
+
+    # print the head of X_train_scaled
+    # print(X_train_scaled[:5])
+
     # convert the data into jax.numpy array
     X_train_scaled = jnp.array(X_train_scaled)
     y_train = jnp.array(y_train)
 
     # define the embedding kernel (with JAX)
-    num_qubits = 6
-    num_layers = 4
+    num_layers = 1
     wires = list(range(num_qubits))
     dev = qml.device('default.qubit.jax', wires=num_qubits)
-
-    # random params or load them from pkl file
-    load_params = True
-
-    if load_params:
-        with open(PICKLE_DATA_DIR / f"q_kern_kta_opt_he2w{num_qubits}d{num_layers}.pkl", 'rb') as params_file:
-            params = pickle.load(params_file)
-    else:
-        _, key = jax.random.split(key)
-        params = jax.random.uniform(
-            key, minval=0., maxval=2*jnp.pi, shape=(num_layers, 2, num_qubits))
 
     @jax.jit
     @qml.qnode(device=dev, interface="jax")
     def kernel(x1, x2):
         """x1, x2 must be JAX arrays"""
-        embedding(x1, params, wires)
-        qml.adjoint(embedding)(x2, params, wires)
+        qml.IQPEmbedding(x1, wires, n_repeats=num_layers, pattern=None)
+        qml.adjoint(qml.IQPEmbedding)(
+            x2, wires, n_repeats=num_layers, pattern=None)
         return qml.expval(qml.Projector([0]*num_qubits, wires=wires))
 
     # Define the parameter grid for GridSearchCV
@@ -98,10 +76,8 @@ if __name__ == '__main__':
     # Extract the mean and standaed deviation of the validation error and save to csv file
     python_file_name = os.path.basename(__file__)
     python_file_name_no_ext = os.path.splitext(python_file_name)[0]
-    file_name_width_depth = python_file_name_no_ext + \
-        f"_he2w{num_qubits}d{num_layers}"
-    results_file_name = file_name_width_depth + \
-        "_trained" if load_params else file_name_width_depth + "_random"
+    results_file_name = python_file_name_no_ext + \
+        f"_iqpw{num_qubits}d{num_layers}"
     df_results.to_csv(
         RESULTS_DIR / f'{results_file_name}.csv', index=False)
 
