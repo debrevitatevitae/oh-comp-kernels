5f9fbb0223c32b1598d7a7c3a4d2e09e9e5b7ed8
debrevitatevitae <giotb92@gmail.com>
2024-01-20 07:26:50 +0100

    Refactor quantum kernel accuracy script to choose between HE2 and IQP kernel

--- scripts/q_kern_accuracy_cv.py
+++ scripts/q_kern_accuracy_cv.py
@@ -11,7 +11,9 @@ import pennylane as qml
 from sklearn.model_selection import GridSearchCV, cross_val_score, train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVC
+
 from ohqk.project_directories import PICKLE_DIR, PROC_DATA_DIR, RESULTS_DIR
+from ohqk.quantum_embeddings import trainable_embedding
 
 
 jax.config.update('jax_enable_x64', False)
@@ -19,6 +21,9 @@ jax.config.update('jax_enable_x64', False)
 
 if __name__ == '__main__':
     start = time.time()
+
+    # whethere the embedding has trained parameters or not
+    trained = True
 
     np.random.seed(42)
     key = jax.random.PRNGKey(42)
@@ -54,22 +59,32 @@ if __name__ == '__main__':
             wires = list(range(num_qubits))
             dev = qml.device('default.qubit.jax', wires=num_qubits)
 
+            if trained:
+                # the embedding is an HEA and we load the trained parameters
+                with open(PICKLE_DIR / f"q_kern_kta_opt_he2w{num_qubits}d{num_layers}.pkl", 'rb') as f:
+                    params = pickle.load(f)
+
+                embedding = partial(trainable_embedding, params=params)
+            else:
+                # the embedding is IQP, which is not trainable
+                embedding = partial(
+                    qml.IQPEmbedding, n_repeats=num_layers, pattern=None)
+
             @jax.jit
             @qml.qnode(device=dev, interface="jax")
             def kernel(x1, x2):
                 """x1, x2 must be JAX arrays"""
-                qml.IQPEmbedding(x1, wires, n_repeats=num_layers, pattern=None)
-                qml.adjoint(qml.IQPEmbedding)(
-                    x2, wires, n_repeats=num_layers, pattern=None)
+                embedding(x1, wires=wires)
+                qml.adjoint(embedding)(x2, wires=wires)
                 return qml.expval(qml.Projector([0]*num_qubits, wires=wires))
 
-            # create SVC and limit the number of iterations to a reasonable number
+            # create SVC
             svc = SVC(kernel=partial(qml.kernels.kernel_matrix,
                       kernel=kernel), cache_size=1000)
 
             # create a GridSearchCV and fit to the data and limit the time for each fit to 10 minutes
             grid_search = GridSearchCV(
-                svc, cv_param_grid, cv=5, n_jobs=4, verbose=3)
+                svc, cv_param_grid, cv=5, n_jobs=7, verbose=3)
 
             grid_search.fit(X_train_scaled, y_train)
 
@@ -79,7 +94,7 @@ if __name__ == '__main__':
             python_file_name = os.path.basename(__file__)
             python_file_name_no_ext = os.path.splitext(python_file_name)[0]
             results_file_name = python_file_name_no_ext + \
-                f"_iqpw{num_qubits}d{num_layers}"
+                f"_he2w{num_qubits}d{num_layers}"
             df_results.to_csv(
                 RESULTS_DIR / f'{results_file_name}.csv', index=False)
 
