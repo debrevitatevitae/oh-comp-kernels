ffb857a8f0b6428a55568e26d4d0113277ccff46
debrevitatevitae <giotb92@gmail.com>
2024-01-28 15:21:58 +0100

    Refactor rbf_accuracy_test.py to better comply to PEP8

--- scripts/rbf_accuracy_test.py
+++ scripts/rbf_accuracy_test.py
@@ -6,22 +6,22 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+
 from ohqk.project_directories import PROC_DATA_DIR, RESULTS_DIR
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     start = time.time()
 
     np.random.seed(42)
 
     # data loading, splitting, and scaling
-    df_data = pd.read_csv(PROC_DATA_DIR / 'data_labeled.csv')
+    df_data = pd.read_csv(PROC_DATA_DIR / "data_labeled.csv")
     df_train, df_test = train_test_split(df_data, test_size=0.2)
 
-    X_train = df_train[['eps11', 'eps22', 'eps12']].to_numpy()
-    y_train = df_train['failed'].to_numpy(dtype=np.int32)
-    X_test = df_test[['eps11', 'eps22', 'eps12']].to_numpy()
-    y_test = df_test['failed'].to_numpy(dtype=np.int32)
+    X_train = df_train[["eps11", "eps22", "eps12"]].to_numpy()
+    y_train = df_train["failed"].to_numpy(dtype=np.int32)
+    X_test = df_test[["eps11", "eps22", "eps12"]].to_numpy()
+    y_test = df_test["failed"].to_numpy(dtype=np.int32)
 
     N = len(y_train)
     scaler = StandardScaler()
@@ -40,8 +40,14 @@ if __name__ == '__main__':
     print(f"Optimal gamma: {gamma_opt}")
 
     # create a DataFrame for the results, which includes the training set size, mean test accuracy, the standard deviation of the test accuracy and the number of support vectors
-    df = pd.DataFrame(columns=[
-                      "train_size", "mean_test_accuracy", "std_test_accuracy", "n_support_vectors"])
+    df = pd.DataFrame(
+        columns=[
+            "train_size",
+            "mean_test_accuracy",
+            "std_test_accuracy",
+            "n_support_vectors",
+        ]
+    )
 
     # declare some training set sizes
     train_sizes = [int(N * frac) for frac in np.arange(0.1, 1.1, 0.1)]
@@ -60,15 +66,17 @@ if __name__ == '__main__':
             clf.fit(X_train_scaled_selection, y_train_selection)
             accuracies.append(clf.score(X_test_scaled, y_test))
 
-        df.loc[len(df)] = {"train_size": ts,
-                           "mean_test_accuracy": np.mean(accuracies),
-                           "std_test_accuracy": np.std(accuracies),
-                           "n_support_vectors": clf.n_support_.sum()}
+        df.loc[len(df)] = {
+            "train_size": ts,
+            "mean_test_accuracy": np.mean(accuracies),
+            "std_test_accuracy": np.std(accuracies),
+            "n_support_vectors": clf.n_support_.sum(),
+        }
 
     # save DataFrame to a csv file named after this Python file
     python_file_name = os.path.basename(__file__)
     python_file_name_no_ext = os.path.splitext(python_file_name)[0]
-    df.to_csv(RESULTS_DIR / f'{python_file_name_no_ext}.csv', index=False)
+    df.to_csv(RESULTS_DIR / f"{python_file_name_no_ext}.csv", index=False)
 
     exec_time = time.time() - start
     minutes = int(exec_time // 60)
