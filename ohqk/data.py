ad7b15561e286da45241aa4c9c4e70921ec9002d
debrevitatevitae <giotb92@gmail.com>
2024-02-03 17:47:21 +0100

    Add LabelledDataset class to ohqk/data.py

---
+++ ohqk/data.py
@@ -0,0 +1,15 @@
+from torch.utils.data import Dataset
+
+
+class LabelledDataset(Dataset):
+    def __init__(self, data, labels):
+        self.data = data
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        x = self.data[index]
+        y = self.labels[index]
+        return x, y
