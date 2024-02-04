0d90be1b9845f6f52cfa5957fb56e31afa018ed1
debrevitatevitae <giotb92@gmail.com>
2024-02-04 11:11:08 +0100

    Add dataset for retrieving strains, stresses and labels from file

--- ohqk/data.py
+++ ohqk/data.py
@@ -32,3 +32,61 @@ def read_file_to_numpy_array(filepath, dim_input=1, sample_size=None):
     return data[:, :dim_input].reshape(-1, dim_input), data[
         :, dim_input:
     ].reshape(-1, dim_output)
+
+
+def assign_labels(strains, stresses, curve_idx=0, residual_stiffness=0.9):
+    """
+    Assigns labels to data points based on the given strains and stresses.
+
+    Parameters:
+        strains (ndarray): Array of strains.
+        stresses (ndarray): Array of stresses.
+        curve_idx (int, optional): Index of the curve to consider. Defaults to 0.
+        residual_stiffness (float, optional): Residual stiffness threshold. Defaults to 0.9.
+
+    Returns:
+        ndarray: Array of labels assigned to each data point.
+    """
+
+    N, _ = stresses.shape
+
+    threshold = N - 1
+    stiffness_init = stresses[0, curve_idx] / strains[0, curve_idx]
+    for i in range(1, N):
+        stiffness = stresses[i, curve_idx] / strains[i, curve_idx]
+        if stiffness <= residual_stiffness * stiffness_init:
+            threshold = i
+            break
+
+    labels = np.array(
+        [-1 if i < threshold else 1 for i in range(N)], dtype=np.int32
+    )
+    return labels
+
+
+class StrainsStressesLabels:
+    """Class for loading and labeling strains and stresses"""
+
+    def __init__(self, data_file_path, labeling_threshold=0.9):
+        self.data_file_path = data_file_path
+        self.labeling_threshold = labeling_threshold
+
+        self.strains, self.stresses = read_file_to_numpy_array(
+            self.data_file_path,
+            dim_input=3,
+        )
+
+        self.labels = assign_labels(
+            self.strains,
+            self.stresses,
+            residual_stiffness=self.labeling_threshold,
+        )
+
+    def __len__(self):
+        return len(self.strains)
+
+    def __getitem__(self, index):
+        x = self.strains[index]
+        y = self.stresses[index]
+        z = self.labels[index]
+        return x, y, z
