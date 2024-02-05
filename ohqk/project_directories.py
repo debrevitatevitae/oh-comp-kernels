5e220ca6c21dacd29ea6573b182c4d7995e3baff
debrevitatevitae <giotb92@gmail.com>
2024-02-05 08:37:58 +0100

    Use project absolute path for compatibility with both scripts and nbs

--- ohqk/project_directories.py
+++ ohqk/project_directories.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 
-DATA_DIR = Path() / 'data'
-RAW_DATA_DIR = DATA_DIR / 'raw'
-PICKLE_DIR = Path() / 'pickle'
-PROC_DATA_DIR = DATA_DIR / 'processed'
-GRAPHICS_DIR = Path() / 'graphics'
-RESULTS_DIR = Path() / 'results'
+PROJECT_ABSOLUTE_PATH = Path(__file__).resolve().parents[1]
+
+DATA_DIR = PROJECT_ABSOLUTE_PATH / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+PICKLE_DIR = PROJECT_ABSOLUTE_PATH / "pickle"
+PROC_DATA_DIR = DATA_DIR / "processed"
+GRAPHICS_DIR = PROJECT_ABSOLUTE_PATH / "graphics"
+RESULTS_DIR = PROJECT_ABSOLUTE_PATH / "results"
